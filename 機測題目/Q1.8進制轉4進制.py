0a2697d37369df915dc87a460da275f85e565dae
luke90329 <32509228+luke90329@users.noreply.github.com>
2020-06-10 11:17:12 +0800

    Update Q1.8進制轉4進制.py

--- 機測題目/Q1.8進制轉4進制.py
+++ 機測題目/Q1.8進制轉4進制.py
@@ -1,13 +1,13 @@
 times = eval(input())
 for i in range(times):
-    num = eval(input())
+    num = input()
     n = 0
     sum = 0
-    for j in str(num)[::-1]:
+    for j in num[::-1]:
         sum += (8**(n))*int(j)
         n += 1
     b =''
-    while sum > 4:
+    while sum >= 4:
         b += str(sum % 4)
         sum = (sum-(sum % 4))//4
     b += str(sum)
